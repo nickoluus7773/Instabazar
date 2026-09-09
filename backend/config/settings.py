1158beb39921dcf844bdc9cb11ebd108be465191
rushigohel76 <gohelrushi50@gmail.com>
2026-09-10 01:27:46 +0530

    feat: add local image storage for vendors and products

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -132,6 +132,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CORS_ALLOW_ALL_ORIGINS = True
